c8515acaac88026c376a2c4c8bc22fe667cf572e
LEEGYUMIN <dlrb1212@gmail.com>
2021-02-07 22:38:52 +0900

    t

---
+++ src/components/HeaderBar.vue
@@ -0,0 +1,15 @@
+<template>
+	<div>
+		<ul class="nav">
+			<li class="back"></li>
+			<li class="page"></li>
+			<li class="hamburger"></li>
+		</ul>
+	</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,45 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+	<div class="main">
+		<div class="container">
+			<div class="nav">
+				<div class="hamburger"></div>
+			</div>
+			<header>
+				<div class="profile">
+					<div class="profile-img"><img src="" alt="" /></div>
+					<div class="desc">
+						<div>
+							안녕하세요. <span>이규민님</span> <br />
+							사용가능한 적립금 <br />
+							<span>4230</span>원<br />입니다.
+						</div>
+					</div>
+				</div>
+			</header>
+			<main>
+				<div class="menu">
+					<div class="pay-history">결제내역</div>
+					<div class="passbook">모임통장</div>
+				</div>
+			</main>
+		</div>
+	</div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
-  name: "Home",
-  components: {
-    HelloWorld
-  }
+	name: 'main',
+	components: {},
 };
 </script>
+
+<style scoped>
+.nav {
+	width: 100%;
+}
+header {
+	width: 100%;
+}
+header .profile {
+}
+</style>
