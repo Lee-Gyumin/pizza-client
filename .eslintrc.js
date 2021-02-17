a82d20e1c0b207afe400d5058eeb8d0c23eb863f
LEEGYUMIN <dlrb1212@gmail.com>
2021-02-17 18:41:46 +0900

    vue.config.js 절대 경로 추가

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,11 +13,11 @@ module.exports = {
         "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
         "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
         'prettier/prettier': [
-            'error',
+            'warn',
             {
                 singleQuote: true,
                 semi: true,
-                useTabs: true,
+                useTabs: false,
                 tabWidth: 2,
                 trailingComma: 'all',
                 printWidth: 80,
