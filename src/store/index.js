aca3e6eeb1339cd00c026d8b3414cefd78ada188
DESKTOP-C6KU3VI\f <dlrb1212@gmail.com>
2021-03-28 22:54:31 +0900

    kk

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,24 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-
+import createPersistedState from 'vuex-persistedstate';
 Vue.use(Vuex);
 
 export default new Vuex.Store({
+    plugins: [
+        createPersistedState({
+          paths: ['token', 'userInfo', 'rememberId', 'autoLogin'],
+        }),
+      ],
     state: {
-        token:'',
+        userInfo: {
+            userEmail: '',
+            userSeq: 0,
+            userName: '',
+            userType: '',
+            companySeq: '',
+          },
+        autoLogin: false,
+        token: '',
     },
     mutations: {},
     actions: {},
